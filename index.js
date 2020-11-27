08e705f0cc7e99f0c0b3e1f71ce9e40733cb5834
muchsin28 <hab@muchsin.id>
2020-11-27 07:20:21 +0700

    Fetch and show data from 3rd Party API Server

--- index.js
+++ index.js
@@ -64,67 +64,67 @@ const register=()=>{
   })
 }
 
-const addData=()=>{
-  let name=$("#todo-name").val()
-  let description=$("#description").val()
-  let status= $("#status").find(":selected").text();
-  let category=$("input[name='category']:checked").val();
-  let due=$("#due").val()
-  $.ajax({
-    url: "http://localhost:3000/todos",
-    method: "POST",
-    headers:{
-      access_token: localStorage.access_token
-    },
-    data:{
-      name,
-      description,
-      status,
-      category,
-      due
-    }
-  })
-  .done(response =>{
-    console.log(response)
+// const addData=()=>{
+//   let name=$("#todo-name").val()
+//   let description=$("#description").val()
+//   let status= $("#status").find(":selected").text();
+//   let category=$("input[name='category']:checked").val();
+//   let due=$("#due").val()
+//   $.ajax({
+//     url: "http://localhost:3000/todos",
+//     method: "POST",
+//     headers:{
+//       access_token: localStorage.access_token
+//     },
+//     data:{
+//       name,
+//       description,
+//       status,
+//       category,
+//       due
+//     }
+//   })
+//   .done(response =>{
+//     console.log(response)
    
-    showMainPage()
-  })
-  .fail((xhr, textStatus)=>{
-    console.log(xhr, textStatus)
-  })
-  .always(()=>{
-    $("#todo-name").val("")
-    $("#description").val("")
-    $("#due").val("")
-  })
-}
+//     showMainPage()
+//   })
+//   .fail((xhr, textStatus)=>{
+//     console.log(xhr, textStatus)
+//   })
+//   .always(()=>{
+//     $("#todo-name").val("")
+//     $("#description").val("")
+//     $("#due").val("")
+//   })
+// }
 
 const getData=()=>{
   $.ajax({
-    url: "http://localhost:3000/todos",
+    url: "http://localhost:3000/museums",
     method: "GET",
     headers:{
       access_token: localStorage.access_token
     }
   })
   .done(response =>{
-    response.forEach(todo => {
-      console.log(todo)
+    response.forEach(data => {
+      console.log(data)
       $("#todos").append(`
       <div class="col-md-4">
         <div class="card mb-4 shadow-sm ">
           <img src="https://www.targettraining.eu/wp-content/uploads/2019/02/meetings-practice.jpg" class="card-img-top" alt="...">
           <div class="card-body text-left">
-            <h5 class="card-title">${todo.name}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">${todo.category}</h6>
-            <h6 class="card-subtitle mb-2 text-muted">Status: ${todo.status}</h6>
-            <p class="card-text">${todo.description}</p>
+            <h5 class="card-title">${data.nama_museum}</h5>
+            <p class="card-subtitle mb-2 text-muted">${data.alamat}</p><hr>
+            <h6 class="card-subtitle mb-2 text-muted">Details:</h6>
+            <p class="card-text">${data.deskripsi}</p>
             <div class="d-flex justify-content-between align-items-center">
               <div class="btn-group">
                 <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                 <button type="button" class="btn btn-sm btn-outline-secondary">Delete</button>
               </div>
-              <small class="text-muted">Due ${todo.due}</small>
+              <small class="text-muted">Due ${data.due}</small>
             </div>
           </div>
         </div>
@@ -167,10 +167,10 @@ $(document).ready(function(){
     showLoginPage()
   })
 
-  $('#add-todo-form').on('submit', (event)=>{
-    event.preventDefault()
-    addData()
-  })
+  // $('#add-todo-form').on('submit', (event)=>{
+  //   event.preventDefault()
+  //   addData()
+  // })
 
   $('#logout-button').on('click',()=>{
     logout()
